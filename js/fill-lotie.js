8a150bc7ae306649fa662fca4346c6d38f3b2b1d
unknown <rootmawar77@gmail.com>
2020-07-28 20:09:21 +0700

    Final version

---
+++ js/fill-lotie.js
@@ -0,0 +1,39 @@
+let animation1 = () => {
+	bodymovin.loadAnimation({
+	  container: document.getElementById('lottie1'),
+	  renderer: 'svg',
+	  loop: true,
+	  autoplay: true,
+	  path: './js/lottie1.json'
+	})
+}
+
+let animation2 = () => {
+	bodymovin.loadAnimation({
+	  container: document.getElementById('lottie2'),
+	  renderer: 'svg',
+	  loop: true,
+	  autoplay: true,
+	  path: './js/lottie2.json'
+	})
+}
+
+let animation3 = () => {
+	bodymovin.loadAnimation({
+	  container: document.getElementById('lottie3'),
+	  renderer: 'svg',
+	  loop: true,
+	  autoplay: true,
+	  path: './js/lottie3.json'
+	})
+}
+
+let animation4 = () => {
+	bodymovin.loadAnimation({
+	  container: document.getElementById('lottie4'),
+	  renderer: 'svg',
+	  loop: true,
+	  autoplay: true,
+	  path: './js/lottie4.json'
+	})
+}
